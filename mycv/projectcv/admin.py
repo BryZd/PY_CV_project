3173819fe42455cc2034bb7cfbe4f9bddb2bb3cd
BryZd <zdenek.brynych@gmail.com>
2025-03-19 22:20:41 +0100

    added: user, superuser (admin)

--- mycv/projectcv/admin.py
+++ mycv/projectcv/admin.py
@@ -1,7 +1,64 @@
 from django.contrib import admin
-from .models import Book, Genre
+from django import forms
+
+
+from .models import Book, Genre, User, UserManager
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+
+
+
+
+class UserCreationForm(forms.ModelForm):
+    password = forms.CharField(label="Password", widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ["email"]
+
+    def save(self, commit=True):
+        if self.is_valid():
+            user = super().save(commit=False)
+            user.set_password(self.cleaned_data["password"])
+            if commit:
+                user.save()
+            return user
+
+
+class UserChangeForm(forms.ModelForm):
+    password = ReadOnlyPasswordHashField()
+
+    class Meta:
+        model = User
+        fields = ["email", "is_admin"]
+
+    def __init__(self, *args, **kwargs):
+        super(UserChangeForm, self).__init__(*args, **kwargs)
+        self.Meta.fields.remove("password")
+
+
+class Admin(UserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    list_display = ["email", "is_admin"]
+    list_filter = ["is_admin"]
+    fieldsets = (
+        (None, {"fields": ["email", "password"]}),
+        ("Permission", {"fields": ["is_admin"]}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            "fields": ["email", "password"]}
+        ),
+    )
+    search_fields = ["email"]
+    ordering = ["email"]
+    filter_horizontal = []
 
 # Register your models here.
 
 admin.site.register(Book)
 admin.site.register(Genre)
+admin.site.register(User, Admin)
--- mycv/projectcv/models.py
+++ mycv/projectcv/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 # Create your models here.
 
@@ -18,5 +19,47 @@ class Book(models.Model):
         return "Title: {0} | Author: {1} | Genre: {2}".format(self.title, self.author, self.genre.genre_name)
 
 
+class UserManager(BaseUserManager):
+    # create user
+    def create_user(self, email, password):
+        print(self.model)
+        if email and password:
+            user = self.model(email=self.normalize_email(email))
+            user.set_password(password)
+            user.save()
+            return user
 
+    # create admin
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+        user.is_admin = True
+        user.save()
+        return user
+
+
+class User(AbstractBaseUser):
+
+    email = models.EmailField(max_length=300, unique=True)
+    is_admin = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = "user"
+        verbose_name_plural = "users"
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+
+    def __str__(self):
+        return "email: {}".format(self.email)
+
+    @property
+    def is_staff(self):
+        return self.is_admin
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
 
