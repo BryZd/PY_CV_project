7009881e39f0401a3bf15690775d88e3f20b75d3
BryZd <zdenek.brynych@gmail.com>
2025-08-11 21:43:34 +0200

    1. fixed
    pozn.: not tested!

--- mycv/projectcv/views.py
+++ mycv/projectcv/views.py
@@ -1,29 +1,37 @@
+from django.http import Http404
 from django.shortcuts import render, redirect, get_object_or_404
-from django.urls import reverse
+from django.template.context_processors import request
+from django.urls import reverse, reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views import generic
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
-from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import Q
+from django.views.generic import FormView
 
 from .models import Book, User, Genre
 from .forms import BookForm, UserForm, LoginForm
+
+
+RATING_CHOICES = [(i, i) for i in range(1, 6)]
+RATING_VALUES = [choice[0] for choice in RATING_CHOICES]
 
 # Create your views here.
 class BookIndex(generic.ListView):
     template_name = "projectcv/book_index.html"  # cesta k šabloně ze složky tamplates (je možné sdílet mezi aplikacemi)
     context_object_name = "books"  # pod tímto jménem budeme volat seznam objektů v šabloně
 
-    # tato metoda nám získává seznam knih seřazených od největšího id (9,8,7...)
+
 
     def get_queryset(self):
+        # Get filtered parameters from request
         qs = Book.objects.select_related("genre").order_by("-id")
         q = self.request.GET.get("q")
         author = self.request.GET.get("author")
         genre = self.request.GET.get("genre")
         rating = self.request.GET.get("rating")
 
+        # Apply filters if parameters exist
         if q:
             qs = qs.filter(Q(title__icontains=q) | Q(author__icontains=q))
         if author:
@@ -36,9 +44,9 @@ class BookIndex(generic.ListView):
 
     def get_context_data(self, **kwargs):
         ctx = super().get_context_data(**kwargs)
-        ctx["authors"] = (Book.objects.order_by("author").distinct().values_list("author", flat=True).distinct())
+        ctx["authors"] = Book.objects.order_by("author").values_list("author", flat=True).distinct()
         ctx["genres"] = Genre.objects.order_by("genre_name")
-        ctx["ratings"] = [1, 2, 3, 4, 5]
+        ctx["ratings"] = RATING_VALUES
         ctx["current"] = {
             "q": self.request.GET.get("q", ""),
             "author": self.request.GET.get("author", ""),
@@ -55,45 +63,48 @@ class CurrentBook(generic.DetailView):
     def get(self, request, *args, **kwargs):
         try:
             book = self.get_object()
-        except:
+        except (Book.DoesNotExist, Http404):
             return redirect("book_index")
         return render(request, self.template_name, {"book": book})
 
     def post(self, request, pk):
         # Only allow admins to trigger edit/delete actions via POST
-        if not request.user.is_authenticated or not getattr(request.user, "is_admin", False)
+        if not request.user.is_authenticated or not request.user.is_staff:
             messages.info(request, "Only the admin can edit books.")
             return redirect("book_index")
 
+        book = self.get_object()
         if "edit" in request.POST:
-            return redirect("edit_book", pk=self.get_object().pk)
+            return redirect("edit_book", pk=book.pk)
         if "delete" in request.POST:
-            self.get_object().delete()
+            book.delete()
             return redirect("book_index")
         return redirect("book_detail", pk=pk)
 
-class AddBook(generic.edit.CreateView):
+class AddBook(UserPassesTestMixin, generic.edit.CreateView):
 
     form_class = BookForm
     template_name = "projectcv/add_book.html"
 
+    def test_func(self):
+        return self.request.user.is_staff
+
+    def handle_no_permission(self):
+        messages.info(self.request, "Only the admin can add books.")
+        return redirect("book_index")
+
 #Metoda pro GET request, zobrazí pouze formulář
     def get(self, request):
-        if not request.user.is_authenticated or not getattr(request.user, "is_admin", False):
-            messages.info(request, "Only the admin can add books.")
-            return redirect("book_index")
         form = self.form_class(None)
         return render(request, self.template_name, {"form": form})
 
 # Metoda pro POST request, zkontroluje formulář; pokud je validní, vytvoří novou knihu; pokud ne, zobrazí formulář s chybovou hláškou
     def post(self, request):
-        if not request.user.is_authenticated or not getattr(request.user, "is_admin", False):
-            messages.info(request, "Only the admin can add books.")
-            return redirect("book_index")
         form = self.form_class(request.POST)
         if form.is_valid():
             book = form.save()
-        return redirect("book_detail", pk=book.pk)
+            return redirect("book_detail", pk=book.pk)
+        return render(request, self.template_name, {"form": form})
 
 class EditBook(LoginRequiredMixin, UserPassesTestMixin, generic.edit.UpdateView):
     model = Book
@@ -103,8 +114,7 @@ class EditBook(LoginRequiredMixin, UserPassesTestMixin, generic.edit.UpdateView)
 
     # Only admin can edit
     def test_func(self):
-        user = self.request.user
-        return user.is_authenticated and getattr(user, "is_admin", False)
+        return self.request.user.is_staff
 
     # Message + redirect when not permitted
     def handle_no_permission(self):
@@ -124,57 +134,44 @@ class UserViewRegister(generic.edit.CreateView):
     form_class = UserForm
     model = User
     template_name = "projectcv/user_form.html"
+    success_url= reverse_lazy("book_index")
 
-    def get(self, request):
+    def dispatch(self, request, *args, **kwargs):
         if request.user.is_authenticated:
             messages.info(request, "You are already logged in, you cannot register.")
             return redirect("book_index")
-        else:
-            form = self.form_class(None)
-        return render(request,self.template_name, {"form": form})
+        return super().dispatch(request, *args, **kwargs)
 
-    def post(self, request):
-        if request.user.is_authenticated:
-            messages.info(request,"You are already logged in; you cannot register.")
-            return redirect("book_index")
-        form = self.form_class(request.POST)
-        if form.is_valid():
-            user = form.save(commit=False)
-            password = form.cleaned_data["password"]
-            user.set_password(password)
-            user.save()
-            login(request, user)
-            return redirect("book_index")
+    def form_valid(self, form):
+        user = form.save(commit=False)
+        password = form.cleaned_data["password"]
+        user.set_password(password)
+        user.save()
+        login(self.request, user)
+        return super().form_valid(form)
 
-        return render(request, self.template_name, {"form": form})
 
-class UserViewLogin(generic.edit.CreateView):
+class UserViewLogin(FormView):
     form_class = LoginForm
     template_name = "projectcv/user_form.html"
+    success_url = reverse_lazy("book_index")
 
-    def get(self, request):
+    def dispatch(self, request, *args, **kwargs):
         if request.user.is_authenticated:
-            messages.info(request, "You are already logged in; you cannot log in again.")
+            messages.info(request, "You are already logged in.")
             return redirect("book_index")
-        else:
-            form = self.form_class(None)
-        return render(request, self.template_name, {"form": form})
+        return super().dispatch(request, *args, **kwargs)
 
-    def post(self, request):
-        if request.user.is_authenticated:
-            messages.info(request, "You are already logged in; you cannot log in again.")
-            return redirect("book_index")
-        form = self.form_class(request.POST)
-        if form.is_valid():
+    def form_valid(self, form):
             email = form.cleaned_data["email"]
             password = form.cleaned_data["password"]
-            user = authenticate(request, username=email, password=password)
+            user = authenticate(self.request, username=email, password=password)
             if user:
-                login(request, user)
-                return redirect("book_index")
+                login(self.request, user)
+                return super().form_valid(form)
             else:
-                messages.error(request, "This account doesn't exist")
-        return render(request, self.template_name, {"form": form})
+                form.add_error(None, "This account doesn't exist")
+            return self.form_invalid(form)
 
 def logout_user(request):
     if request.user.is_authenticated:
