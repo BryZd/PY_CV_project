db6413ef0cee2e656d1a7883bbfb8670073ff3c8
BryZd <zdenek.brynych@gmail.com>
2025-08-15 22:37:51 +0200

    1. Book Detail completed 2. Everything is showed up but wrong position
    note: need to fix positions of containers

--- mycv/mycv/urls.py
+++ mycv/mycv/urls.py
@@ -18,6 +18,8 @@ from xml.etree.ElementInclude import include
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from . import url_handlers
 
 urlpatterns = [
@@ -25,3 +27,6 @@ urlpatterns = [
     path("", url_handlers.index_handler),
     path("projectcv/", include("projectcv.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
